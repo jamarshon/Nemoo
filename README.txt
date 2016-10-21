cf844e2fd2c708608eed1c965fb8c35eba6df9f6
jamarshon <jasonl96@gmail.com>
2016-10-21 00:45:37 -0400

    hopefully last push

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 Production
-
+heroku config:set 
 npm run minify
 npm run minify -i
 
--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: JSON.stringify(err)
+    error: {}
   });
 });
 
