ccb5bc2b0f7ebd9162c0b09e978ff3f5be872633
jamarshon <jasonl96@gmail.com>
2016-10-21 00:40:38 -0400

    forget

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: JSON.stringify(err)
   });
 });
 
