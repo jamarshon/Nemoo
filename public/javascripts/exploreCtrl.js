1cc4d8d195a1bdbeae863b48b5b708f5a9738376
jamarshon <jasonl96@gmail.com>
2016-11-26 15:55:46 -0500

    Fixing explorepage

--- public/javascripts/exploreCtrl.js
+++ public/javascripts/exploreCtrl.js
@@ -3,54 +3,51 @@ var app = angular.module('App');
 app.controller('ExploreCtrl', ['$http', '$window', '$scope', '$timeout', 'optimizationService',
 											function($http, $window, $scope, $timeout, optimizationService){
 	var that = this;
-	var size = 200;
-	this.size = size + 30;
-	this.offset = size/2 + 5;
-
 	$window.document.title = 'Nemoo - Explore';
-	
+	that.loaded = -1;
+
 	$http.get('/googleTrends').then(function(res){
 		that.items = res.data;
+		that.loaded = 1;
 	}, function(){
 		that.items = [];
+		that.loaded = 1;
 	});
 
-	var resizeHandler = $scope.$watch(function(){ return $('#explore-page').width(); }, function(newVal, oldVal){
-		if(newVal !== oldVal) {
-			that.maxTiles = Math.floor(newVal / that.size);
-			that.margin = newVal / (2 * that.maxTiles) - that.offset;
-			console.log(that.margin);
-			$timeout(function(){
-				if(!that.items) { return; }
-				var start, j, max;
-				var i = 0, len = that.items.length;
-				that.heights = _.map($('.explore-tile'), function(n){ return $(n).height(); });
-				that.roundedHeights = [];
-				while(i < len) {
-					max = 0;
-					start = i;
-					i = Math.min(i + that.maxTiles, len);
-					for(j = start; j < i; j++) {
-						if(max < that.heights[j]) max = that.heights[j];
-					}
-					that.roundedHeights.push(max);
-				}
-			});
-		}
+	var resizeHandler = $scope.$watch(function(){ return $('#explore-page').width() * that.loaded; }, function(newVal, oldVal){
+		$timeout(function(){
+			if(!that.items || that.items.length === 0) { return; }
+			var dummyItemEl;
+			var explorePageEl = $('#explore-page');
+
+			// Remove previous dummy elements
+			explorePageEl.find('.dummy-explore').remove();
+
+			var offSetTopMap = _.groupBy(explorePageEl.children(), function(n){ return n.offsetTop; }),
+					numRows = Object.keys(offSetTopMap).length;
+
+			if(numRows < 2){ return; } // Since there is less than two rows we don't need to worry about uneven spacings
+
+			var dummyItemLen = 0;
+					prevHeight = explorePageEl[0].clientHeight,
+					currHeight = prevHeight;
+
+			// Keep adding dummy items until the height increases
+			while(currHeight === prevHeight) {
+				dummyItemEl = $('<div>', {class: 'explore-tile dummy-explore', style: 'visibility: hidden'});
+				explorePageEl.append(dummyItemEl);
+				currHeight = explorePageEl[0].clientHeight;
+				dummyItemLen++;
+			}
+
+			explorePageEl.children().last().remove();
+		});
 	});
 
 	var destroyHandler = $scope.$on('$destroy', function(){
 		resizeHandler();
 		destroyHandler();
 	});
-
-	this.getHeight = function(index){
-		if(that.roundedHeights && that.roundedHeights.length) {
-			var row = Math.floor(index / that.maxTiles);
-			var diff = that.roundedHeights[row] - that.heights[index];
-			return that.roundedHeights[row] + 'px';
-		}
-	};
 
 	// Creates discussion if it doesn't exist already
 	this.handleClick = function(item, event) {
--- routes/routes.js
+++ routes/routes.js
@@ -97,12 +97,19 @@ module.exports = function(app, passport, isProduction) {
         var items = channel[0].item;
         var data = items.reduce(function(memo, item){
           var e = {};
-          if(item.title.length === 0){ return memo; }
+          if(item.title.length === 0 || item.title[0] === ''){ return memo; }
           e.name = item.title[0];
-          if(item['ht:picture'].length === 0){ return memo; }
-          e.picture = item['ht:picture'][0];
-          if(item['ht:news_item'].length === 0 || item['ht:news_item'][0]['ht:news_item_title'].length === 0){ return memo; }
-          e.description = item['ht:news_item'][0]['ht:news_item_title'][0];
+
+          var pictures = item['ht:picture'];
+          if(pictures.length === 0 || pictures[0] === ''){ return memo; }
+          e.picture = pictures[0];
+
+          var news = item['ht:news_item'];
+          if(news.length === 0){ return memo; }
+          var newsTitle = news[0]['ht:news_item_title'];
+          if(newsTitle.length === 0 || newsTitle[0] === ''){ return memo; }
+          e.description = newsTitle[0];
+          
           memo.push(e);
           return memo;
         }, []);
